2bedefa8266baeb1e96d0dfc5800463e1440fab6
ahadsts9901 <ahadsts990@gmail.com>
2024-07-04 16:59:16 +0500

    ex

--- practice_set/chapter_4/ex_2.py
+++ practice_set/chapter_4/ex_2.py
@@ -1,9 +1,16 @@
-_letter = """
-Dear <Name> 
-    you are invited on 
-<Date>
-"""
+_marks = []
 
-_letter.replace("<Name>", "Abdul Ahad").replace("<Date>", "12/12/2024")
+_mark_1 = int(input("enter mark 1"))
+_marks.append(_mark_1)
+_mark_2 = int(input("enter mark 2"))
+_marks.append(_mark_2)
+_mark_3 = int(input("enter mark 3"))
+_marks.append(_mark_3)
+_mark_4 = int(input("enter mark 4"))
+_marks.append(_mark_4)
+_mark_5 = int(input("enter mark 5"))
+_marks.append(_mark_5)
+_mark_6 = int(input("enter mark 6"))
+_marks.append(_mark_6)
 
-print(_letter)
+print(_marks.sort())
