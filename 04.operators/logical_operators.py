e3542d8dce2e9447370d0ddf1f0816532195fa6c
ahadsts9901 <ahadsts990@gmail.com>
2024-07-04 14:50:31 +0500

    operators

---
+++ 04.operators/logical_operators.py
@@ -0,0 +1,16 @@
+# and operator (and)
+
+a = 5
+b = 8
+
+print(a == 5 and b == 8)
+print(a == 5 and b == 4)
+
+
+# or operator (or)
+
+a = 5
+b = 8
+
+print(a == 5 or b == 8)
+print(a == 3 or b == 4)
