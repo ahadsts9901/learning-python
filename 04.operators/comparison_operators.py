8a23feb733338577aa480c9877bde1d934744ea4
ahadsts9901 <ahadsts990@gmail.com>
2024-07-04 14:48:31 +0500

    comparison-operators

---
+++ 04.operators/comparison_operators.py
@@ -0,0 +1,21 @@
+# assignment operator (=)
+
+a = 1
+
+# double equals for value checking (==)
+
+print(a == a)
+
+# triple equals doean't exist in python (===)
+
+# greater than (>)
+
+print(4>7)
+
+# less than (<)
+
+print(4<7)
+
+# not operator (!)
+
+print(4 != 7)
