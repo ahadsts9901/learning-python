601dbc1cab1439b77ad73fa5d6f53bc40667442e
ahadsts9901 <ahadsts990@gmail.com>
2024-07-04 14:58:15 +0500

    types

---
+++ 05.types/types.py
@@ -0,0 +1,21 @@
+_string = "string"
+_num = 123
+_floating_point_number = 23.6
+_list = [1,2,3,"hello", True]
+_none = None
+_dictionary = {
+    "name": "ahad",
+    "age": 19,
+    "languages": ["python", "typescript", "javascript"]
+}
+_set = {1,2,3,"hello",False,None}
+_tuple = (1,2,"tuple", True,None)
+
+print(type(_string))                    # <class 'str'>
+print(type(_num))                       # <class 'int'>
+print(type(_floating_point_number))     # <class 'float'>
+print(type(_list))                      # <class 'list'>
+print(type(_none))                      # <class 'NoneType'>
+print(type(_dictionary))                # <class 'dict'>
+print(type(_set))                       # <class 'set'>
+print(type(_tuple))                     # <class 'tuple'>
