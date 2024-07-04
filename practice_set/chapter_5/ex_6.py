ba6e34d5685c08f0f28a50abeb2e970a87da50e3
ahadsts9901 <ahadsts990@gmail.com>
2024-07-04 17:21:04 +0500

    ex

---
+++ practice_set/chapter_5/ex_6.py
@@ -0,0 +1,31 @@
+_dict = {}
+
+_name = input("enter your name")
+_lang = input("enter your favourite language")
+
+_dict.update({
+    _name: _lang
+})
+
+_name = input("enter your name")
+_lang = input("enter your favourite language")
+
+_dict.update({
+    _name: _lang
+})
+
+_name = input("enter your name")
+_lang = input("enter your favourite language")
+
+_dict.update({
+    _name: _lang
+})
+
+_name = input("enter your name")
+_lang = input("enter your favourite language")
+
+_dict.update({
+    _name: _lang
+})
+
+print(_dict)
