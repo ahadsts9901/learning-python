be521fc929e195df3d20f4ae4df3286c8d7678ec
ahadsts9901 <ahadsts990@gmail.com>
2024-07-04 14:44:04 +0500

    math-operators

---
+++ 04.operators/math_operators.py
@@ -0,0 +1,42 @@
+# add
+
+a = 6
+b = 8
+c = a+b
+
+print(f"{a} + {b} = {c}")
+
+
+# subtract
+
+d = a-b
+
+print(f"{a} - {b} = {d}")
+
+
+# multiply
+
+e = a*b
+
+print(f"{a} * {b} = {e}")
+
+
+# divide
+
+f = a/b
+
+print(f"{a} / {b} = {f}")
+
+
+# modulus
+
+g = a%b
+
+print(f"{a} % {b} = {g}")
+
+
+# square
+
+h = a**a
+
+print(f"square of  {a} is {h}")
