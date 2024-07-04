b5b47a505a37d6aba04e89a4b3465e6e00c2248e
ahadsts9901 <ahadsts990@gmail.com>
2024-07-04 15:42:03 +0500

    tuple-methods

--- 08.tuples/methods.py
+++ 08.tuples/methods.py
@@ -1,5 +1,15 @@
 _tuple = (1, "str", 90, False, None)
 _nums = (1, 4, 2, 7, 44, 90, 23, 76)
 
-print(_tuple)  # [1, 'str', 90, False, None]
+# Printing the tuple
+print(_tuple)  # Output: (1, 'str', 90, False, None)
 
+# splitting the tuple
+_splitted_tuple = _tuple[0:3]
+print(_splitted_tuple) # Output : (1, "str", 90)
+
+# Counting occurrences of a value in the tuple
+print(_tuple.count(False))  # Output: 1 (count of False)
+
+# Finding the index of an element in the tuple
+print(_tuple.index(90))  # Output: 2 (index of 90 in the tuple)
