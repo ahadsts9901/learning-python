9be3fb6c03d0c8118a38c288de696ba9f9bccbc7
ahadsts9901 <ahadsts990@gmail.com>
2024-07-04 17:24:25 +0500

    ex

---
+++ practice_set/chapter_5/ex_8.py
@@ -0,0 +1,31 @@
+_dict = {}
+
+_name = input("enter your name unique from previous names")
+_lang = input("enter your favourite language ")
+
+_dict.update({
+    _name: _lang
+})
+
+_name = input("enter your name unique from previous names")
+_lang = input("enter your favourite language same as previous")
+
+_dict.update({
+    _name: _lang
+})
+
+_name = input("enter your name unique from previous names")
+_lang = input("enter your favourite language same as previous")
+
+_dict.update({
+    _name: _lang
+})
+
+_name = input("enter your name unique from previous names")
+_lang = input("enter your favourite language same as previous")
+
+_dict.update({
+    _name: _lang
+})
+
+print(_dict)
