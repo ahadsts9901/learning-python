2a37ef46671bb863246c3788661154a4f51719c8
ahadsts9901 <ahadsts990@gmail.com>
2024-07-04 15:38:13 +0500

    tuple

---
+++ 08.tuples/methods.py
@@ -0,0 +1,5 @@
+_tuple = (1, "str", 90, False, None)
+_nums = (1, 4, 2, 7, 44, 90, 23, 76)
+
+print(_tuple)  # [1, 'str', 90, False, None]
+
---
+++ 08.tuples/tuples.py
@@ -0,0 +1,7 @@
+_tuple = (1,2,"hello", True, None)
+
+print(_tuple)
+print(_tuple[1])
+print(_tuple[-1])
+print(_tuple[0:3])
+print(_tuple[0:3:2])
