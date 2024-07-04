64a6d4a23be6d3470bff14e6bc5cc7d98c906b99
ahadsts9901 <ahadsts990@gmail.com>
2024-07-04 14:39:08 +0500

    multi-print

--- 01.hello-world/main.py
+++ 01.hello-world/main.py
@@ -1,1 +1,13 @@
 print("hello world")
+
+
+print('''
+        hello world
+        hello world
+        hello world
+        hello world
+        hello world
+        hello world
+        hello world
+        hello world
+''')
