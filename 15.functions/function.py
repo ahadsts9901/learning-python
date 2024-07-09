bca6fd9ee97718d53e0e5cc0409dcbb5fdeae12c
ahadsts9901 <ahadsts990@gmail.com>
2024-07-09 17:12:38 +0500

    start-game

--- 15.functions/function.py
+++ 15.functions/function.py
@@ -10,3 +10,11 @@ print(sum(1,4))
 # lambda (like arrow functions / anonymous functions)
 # syntax ==> lambda arguments: expression
 sum = lambda a, b: a + b
+
+
+
+# list/array methods
+
+# points = [(1, 2), (3, 1), (5, -1), (4, 2)]
+# points.sort(key=lambda point: point[1])
+# print(points)
