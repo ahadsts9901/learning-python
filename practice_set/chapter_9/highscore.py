6f5b7045032571c99358efb2dd4d52a7cd566a49
ahadsts9901 <ahadsts990@gmail.com>
2025-01-08 01:11:07 +0500

    gane

--- practice_set/chapter_9/highscore.py
+++ practice_set/chapter_9/highscore.py
@@ -1,3 +1,4 @@
+
 # new_score = input("Enter your new score: ")
 
 # with open("highscore.txt","r") as f:
@@ -7,4 +8,21 @@
 #             n.write(new_score)
 
 
+import random
 
+def game():
+    print("you are playing the game")
+    new_score = random.randint(1,100)
+    with open("highscore.txt") as f:
+        hiscore = f.read()
+
+    if(hiscore == ""):
+        hiscore = 0
+    
+    if(int(new_score) > int(hiscore)):
+        with open("highscore.txt", "w") as n:
+            n.write(str(new_score))
+    return new_score
+
+
+game()
