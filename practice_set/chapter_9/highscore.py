b45180a4ca341f2a105809396be0fb6d30706c7e
ahadsts9901 <ahadsts990@gmail.com>
2025-01-08 00:57:03 +0500

    code

---
+++ practice_set/chapter_9/highscore.py
@@ -0,0 +1,10 @@
+# new_score = input("Enter your new score: ")
+
+# with open("highscore.txt","r") as f:
+#     highscore = f.read()
+#     if(new_score > highscore):
+#         with open("highscore.txt", "w") as n:
+#             n.write(new_score)
+
+
+
