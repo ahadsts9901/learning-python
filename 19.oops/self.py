976673560608dcac1f7dd91daaf0aa7775a9103f
ahadsts9901 <ahadsts990@gmail.com>
2025-01-08 12:00:43 +0500

    gree

---
+++ 19.oops/self.py
@@ -0,0 +1,15 @@
+class Employee:
+    language = "javascript",
+    salary = 1200000
+    def get_info(self):
+        print(f"salary is {self.salary} and language is {self.language}")
+    
+    def greet(self):
+        print("hello world")
+
+user = Employee()
+
+user.get_info() # same behaviour
+Employee.get_info(user) # same behaviour
+
+user.greet()
