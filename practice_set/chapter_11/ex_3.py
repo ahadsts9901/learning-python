2e94703c85654026a76f4f92a1b738fa45dcf46b
ahadsts9901 <ahadsts990@gmail.com>
2025-01-08 19:48:49 +0500

    done finally

---
+++ practice_set/chapter_11/ex_3.py
@@ -0,0 +1,21 @@
+class Employee:
+
+    salary = 80000
+    increment = 25
+
+    @property
+    def salaryAfterIncrement(self):
+        return (self.salary + (self.salary * (self.increment / 100)))
+    
+    @salaryAfterIncrement.setter
+    def salaryAfterIncrement(self, val):
+        self.increment = (val - self.salary)
+
+
+e = Employee()
+
+print(e.salary)
+print(e.increment)
+print(e.salaryAfterIncrement)
+e.salaryAfterIncrement = 120000
+print(e.increment)
