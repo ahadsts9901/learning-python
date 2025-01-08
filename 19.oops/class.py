48b75e6d8740a83b1fc884b6ce60b0e0f0021544
ahadsts9901 <ahadsts990@gmail.com>
2025-01-08 11:50:54 +0500

    yes

--- 19.oops/class.py
+++ 19.oops/class.py
@@ -1,8 +1,12 @@
 class Employee:
-    name = "Ahad"
-    salary = "1000000"
-    language = ["py", "ts", "js"]
+    salary = "1000000" # class attribute
+    language = ["py", "ts", "js"] # class attribute
 
 mani = Employee()
 
+mani.name = "Ahad" # object attribute || instance attribute
+
 print(mani.name, mani.salary, mani.language)
+
+
+# here name is object attribute and salary & languuage are class attribute as they directly belongs to the class
