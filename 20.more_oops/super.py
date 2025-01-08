3a135f6424f777644cb82ea77b5903e8726b7695
ahadsts9901 <ahadsts990@gmail.com>
2025-01-08 17:51:49 +0500

    code

--- 20.more_oops/super.py
+++ 20.more_oops/super.py
@@ -10,14 +10,19 @@ class Coder(SecurityGuard):
         super().__init__() # runs the parent constructor also
         print("coder constructor")
 
+    @staticmethod
+    def get_test():
+        print("i am running")
+
+
 
 
 class Officer(Coder):
 
     def __init__(self):
         super().__init__() # runs the parent constructor also
+        Coder().get_test()
         print("officer constructor")
-
 
 security_guard = SecurityGuard()
 coder = Coder()
