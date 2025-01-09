e02131836f113bd4b1cef64318b6be2a971d4dfb
ahadsts9901 <ahadsts990@gmail.com>
2025-01-09 12:40:05 +0500

    cod

--- 22.advanced_python_01/02.types.py
+++ 22.advanced_python_01/02.types.py
@@ -1,6 +1,23 @@
+from typing import List, Union, Tuple, Dict
+
+
 num : int = 67
+
 
 name : str = "hello world"
 
-def sum(a:int,b:int) -> int:
+
+
+def sum(a:int,b:int) -> int:    
     return a+b
+    
+numbers : List[int] = [1,2,3,4,5]
+
+my_tuple : Tuple[int,str] = (12,"string")
+
+my_dict = Dict[str, int] = {
+    "name": 123,
+    "age": 234
+}
+
+my_union : Union[str, int] = "this is a string"
