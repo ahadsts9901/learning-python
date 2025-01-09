6ae3357257d6560084bbe7f08507fa6f84af49e2
ahadsts9901 <ahadsts990@gmail.com>
2025-01-09 22:01:02 +0500

    code

---
+++ practice_set/chapter_13/ex_2.py
@@ -0,0 +1,8 @@
+name = input("name: ")
+marks = input("marks: ")
+phone_number = input("phone_number: ")
+
+exp = "my name is {}, I have {} marks, and my phone number is {}".format(name, marks, phone_number)
+
+print(exp)
+
