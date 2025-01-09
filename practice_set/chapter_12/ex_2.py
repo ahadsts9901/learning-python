2b627a286c5aa3dab7d7088c0f56ba194181778f
ahadsts9901 <ahadsts990@gmail.com>
2025-01-09 19:09:42 +0500

    code

--- practice_set/chapter_12/ex_2.py
+++ practice_set/chapter_12/ex_2.py
@@ -1,8 +1,9 @@
 l = [1,2,3,4,5,6,7,8,9,0]
 
 for index, item in enumerate(l) :
-    if(index == 6):
-        print(f"item 7 is {item}")
+    if index == 3 or index == 5 or index == 7:
+        print(f"item {index} is {item}")
 
 
 print("thank you")
+
