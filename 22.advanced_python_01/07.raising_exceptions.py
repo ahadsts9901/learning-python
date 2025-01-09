e8f573e4f46a154eb8ee0400802ef5d1ed213f1a
ahadsts9901 <ahadsts990@gmail.com>
2025-01-09 17:41:41 +0500

    raise

--- 22.advanced_python_01/07.raising_exceptions.py
+++ 22.advanced_python_01/07.raising_exceptions.py
@@ -1,1 +1,9 @@
-# in progress
+a = int(input("enter first number: "))
+b = int(input("enter second number: "))
+
+if (b == 0) :
+    raise ZeroDivisionError("hey zero is not allowed")
+else:
+    print(f"the division is {a/b}")
+
+    
