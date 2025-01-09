0ba346b57afa84f36b4090e2939c6199227e345b
ahadsts9901 <ahadsts990@gmail.com>
2025-01-09 19:22:02 +0500

    code

--- practice_set/chapter_12/ex_5.py
+++ practice_set/chapter_12/ex_5.py
@@ -3,6 +3,6 @@ n = int(input("Enter a number: "))
 table = [ n * i for i in range(1 ,11) ]
 
 with open("tables.txt", "a") as f :
-    f.write(str(table) + "\n")
+    f.write(f"Table of {n} ===> {str(table)} \n")
 
     
