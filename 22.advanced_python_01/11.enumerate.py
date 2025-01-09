6ce39435bf5db3d4414c4c886589881c097fa8a3
ahadsts9901 <ahadsts990@gmail.com>
2025-01-09 18:58:11 +0500

    code

---
+++ 22.advanced_python_01/11.enumerate.py
@@ -0,0 +1,11 @@
+l = [1,5,6,3,7]
+
+# index = 0
+# for i in l:
+#     print(f"index {index} is {i}")
+#     index += 1
+
+# this can be simplified using enumerate function
+
+for index, item in enumerate(l):
+    print(f"index {index} is {item}")
